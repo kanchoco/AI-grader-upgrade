9e94c4aa260ccafd90e63e88cd576c0c3e068b48
배다빈 <dabin@baedabin-ui-MacBookAir.local>
2026-03-04 13:52:57 +0900

    fix adress

--- frontend/components/LoginScreen.tsx
+++ frontend/components/LoginScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Login.css';
-import bloodReportImg from './public/blood-report.png';
+import bloodReportImg from '../public/blood-report.png';
 
 interface LoginProps {
   apiUrl: string;
